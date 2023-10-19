9706baec89b4ec1a8d70007ffc36721f4a4e9529
Ridoy Sharif <developersridoy@gmail.com>
2023-10-19 19:24:26 +0600

    vercel added

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ const cors  = require("cors")
 const stripe = require("stripe")(process.env.REACT_PAYMENT_SECRET_KEY);
 const port = process.env.PORT || 5000;
 
-
 // middle ware
 app.use(cors())
 app.use(express.json())
@@ -41,8 +40,8 @@ async function run() {
 
 /*========================= products all apis =========================*/
     app.get("/products",async(req,res)=>{
-      const result = await productsCollection.find().toArray();
-      res.send(result);
+      const result = await productsCollection.find({ status: 'approved' }).toArray();
+      res.send(result)
     })
 
     app.get("/products/:id",async(req,res)=>{
@@ -58,14 +57,15 @@ async function run() {
       res.send(result)
     })
 
+/*========================= seller product all apis =========================*/
     app.post('/seller_product',async(req,res)=>{
       const item = req.body;
-      const result = await sellerProductCollection.insertOne({...item,status:"pending"});
+      const result = await productsCollection.insertOne({...item,status:"pending"});
       res.send(result)
     })
 
     app.get('/seller_product',async(req,res)=>{
-      const result = await sellerProductCollection.find().toArray();
+      const result = await productsCollection.find().toArray();
       res.send(result);
     })
 
@@ -77,7 +77,7 @@ async function run() {
           status:"approved"
         }
       }
-      const result = await sellerProductCollection.updateOne(filter, updateDoc);
+      const result = await productsCollection.updateOne(filter, updateDoc);
       res.send(result);
     })
 
@@ -104,7 +104,8 @@ async function run() {
           product_description:body.product_description
         }
       }
-      const result = await productsCollection.updateOne(filter, updateDoc)
+      const result = await productsCollection.updateOne(filter, updateDoc);
+      res.send(result);
     })
 
     app.delete("/products/:id",async(req,res)=>{
@@ -113,7 +114,6 @@ async function run() {
       const result = await productsCollection.deleteOne(query);
       res.send(result);
     })
-
 
 /*========================= add to carts all apis =========================*/
     app.get("/carts",async(req,res)=>{
